d1ea55b50a05aed5c3d6fd248fdb6ea3b4ac540e
hasan <hsanromadon@gmail.com>
2022-08-03 22:40:53 +0700

    feat: edit profile page

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
   import { Router, Route } from 'svelte-routing';
   import Home from './home.svelte';
+  import EditProfile from './edit-profile.svelte';
+
   export let url = '';
 </script>
 
 <Router {url}>
   <Route path="/" component={Home} />
+  <Route path="/profile" component={EditProfile} />
 </Router>
