f1ef5c4c8dfc93159d5b02f2c49ab1af95127d2a
hasan <hsanromadon@gmail.com>
2022-08-03 22:43:11 +0700

    feat: view profile page

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,14 @@
   import { Router, Route } from 'svelte-routing';
   import Home from './home.svelte';
   import EditProfile from './edit-profile.svelte';
-
+  import Profile from './profile.svelte';
   export let url = '';
 </script>
 
 <Router {url}>
   <Route path="/" component={Home} />
   <Route path="/profile" component={EditProfile} />
+  <Route path=":username" let:params>
+    <Profile params={params?.username} />
+  </Route>
 </Router>
