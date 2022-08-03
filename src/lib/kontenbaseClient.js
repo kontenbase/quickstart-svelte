0feef87a9a4bb9baed1ba65c86e0c3441fde4f07
hasan <hsanromadon@gmail.com>
2022-08-03 22:49:52 +0700

    chore: add env variable

--- src/lib/kontenbaseClient.js
+++ src/lib/kontenbaseClient.js
@@ -1,7 +1,5 @@
 import { createClient } from '@kontenbase/sdk';
 
-// @ts-ignore
-const kontenbaseApiKey = '';
 export const kontenbase = createClient({
-  apiKey: '0c3a64ad-957c-4d9f-bf8d-75222d4ca2ad',
+  apiKey: import.meta.env.VITE_SVELTE_APP_KONTENBASE_API_KEY,
 });
